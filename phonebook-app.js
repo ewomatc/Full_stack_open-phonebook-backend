39e32b80003cf10800522a4f9f3a65d09d0e40e2
Great Diro <iamgreatdiro@gmail.com>
2023-04-28 06:38:54 +0100

    update phonebook app.js

--- phonebook-app.js
+++ phonebook-app.js
@@ -29,13 +29,7 @@ app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
 
   const person = persons.find(person => person.id === id)
-  //send a person response if person id exists, send a 404 if it doesnt
-  if(person) {
-    res.json(person)
-    console.log(`request made for note: ${person.id}`);
-  } else {
-    res.status(404).end()
-  }
+  
 })
 
 //delete a single phonbook entry by id
@@ -44,7 +38,7 @@ app.delete('/api/persons/:id', (req, res) => {
 
   Person.findByIdAndRemove(id)
     .then(res.status(204).end())
-    .catch(err => {console.log(err.message)})
+    .catch(err => next(err))
 })
 
 //post a new phonebook entry
